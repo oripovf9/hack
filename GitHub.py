b194a4bd1c8bcd42ac187ba402f14c6934372d9a
oripovf9 <151760313+oripovf9@users.noreply.github.com>
2023-11-22 10:09:32 -0500

    Add files via upload

---
+++ GitHub.py
@@ -0,0 +1,11 @@
+"""son=int(input("Yoshinggizni kiriting : "))
+if son <= 17 :
+    print("Pasport ololmesiz")
+elif son >= 18 :
+    print("Pasport olingan")
+else :
+    print("Nimadir xato")"""
+son1 = range(3, 30, 3)
+for son2 in son1:
+  print(son2)
+
